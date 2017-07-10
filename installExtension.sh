61af460a3c8aa3f90edc63298d3e0559b7859c72
n4cl <devn4cl@gmail.com>
2017-07-11 00:28:01 +0900

    Add installExtension.sh

---
+++ installExtension.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+code --install-extension vscodevim.vim
+code --install-extension mkaufman.HTMLHint
+code --install-extension dbaeumer.vscode-eslint 
+code --install-extension mosapride.zenkaku
