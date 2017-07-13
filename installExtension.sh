145aba3449d6bd9cc7d6d3df0d0ecccc57290cc2
n4cl <devn4cl@gmail.com>
2017-07-14 00:51:07 +0900

    Add installation process for extension

--- installExtension.sh
+++ installExtension.sh
@@ -2,5 +2,6 @@
 
 code --install-extension vscodevim.vim
 code --install-extension mkaufman.HTMLHint
+code --install-extension raymondcamden.CSSLint
 code --install-extension dbaeumer.vscode-eslint 
 code --install-extension mosapride.zenkaku
