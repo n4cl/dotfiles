01162a9bc29120d36015c8b974e874b950739ad4
N4CL <devn4cl@gmail.com>
2026-02-04 23:54:29 +0900

    Delete VSCodeExtension.sh

--- VSCodeExtension.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install VScode extesion
-code --install-extension vscodevim.vim
-code --install-extension mkaufman.HTMLHint
-code --install-extension raymondcamden.CSSLint
-code --install-extension dbaeumer.vscode-eslint 
-code --install-extension mosapride.zenkaku
-code --install-extension itryapitsin.scala
-code --install-extension coenraads.bracket-pair-colorizer
